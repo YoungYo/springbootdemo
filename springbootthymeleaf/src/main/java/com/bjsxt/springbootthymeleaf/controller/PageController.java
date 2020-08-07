0749561bc117eaa9b96f46af55031aef89617d8b
wanghao20 <wanghao20@baijiahulian.com>
2020-08-07 23:29:37 +0800

    添加了Users类

--- springbootthymeleaf/src/main/java/com/bjsxt/springbootthymeleaf/controller/PageController.java
+++ springbootthymeleaf/src/main/java/com/bjsxt/springbootthymeleaf/controller/PageController.java
@@ -1,10 +1,13 @@
 package com.bjsxt.springbootthymeleaf.controller;
 
+import com.bjsxt.springbootthymeleaf.pojo.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class PageController {
@@ -13,6 +16,14 @@ public class PageController {
     public String showPage(Model model){
         model.addAttribute("msg", "床前明月光，疑似地上霜。");
         model.addAttribute("date", new Date());
+        model.addAttribute("gender", "女");
+        model.addAttribute("id", 5);
+        List<Users> list = new ArrayList<>();
+        list.add(new Users("1", "admin", 23));
+        list.add(new Users("2", "kevin", 24));
+        list.add(new Users("3", "lily", 25));
+        list.add(new Users("4", "lucy", 26));
+        model.addAttribute("list", list);
         return "index";
     }
 }
