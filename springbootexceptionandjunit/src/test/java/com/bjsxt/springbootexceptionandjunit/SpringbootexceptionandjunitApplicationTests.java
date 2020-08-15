4b12673b75851ec0f49d9563bab7f17619e23d8f
wanghao20 <wanghao20@baijiahulian.com>
2020-08-15 18:29:07 +0800

    SpringBoot整合JUnit单元测试

--- springbootexceptionandjunit/src/test/java/com/bjsxt/springbootexceptionandjunit/SpringbootexceptionandjunitApplicationTests.java
+++ springbootexceptionandjunit/src/test/java/com/bjsxt/springbootexceptionandjunit/SpringbootexceptionandjunitApplicationTests.java
@@ -3,7 +3,11 @@ package com.bjsxt.springbootexceptionandjunit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// 如果项目只有一个启动类，则该注解不需要指定启动类
 @SpringBootTest
+
+// 如果有多个启动类，则需要指定其中的某一个作为启动类进行测试
+//@SpringBootTest(classes = SpringbootexceptionandjunitApplication.class)
 class SpringbootexceptionandjunitApplicationTests {
 
     @Test
