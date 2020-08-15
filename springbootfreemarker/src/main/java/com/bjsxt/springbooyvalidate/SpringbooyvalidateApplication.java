1fe74aeab7d65df5f716f40b387d0a145ab9061e
wanghao20 <wanghao20@baijiahulian.com>
2020-08-15 18:52:13 +0800

    SpringBoot服务端数据校验demo项目

---
+++ springbootfreemarker/src/main/java/com/bjsxt/springbooyvalidate/SpringbooyvalidateApplication.java
@@ -0,0 +1,13 @@
+package com.bjsxt.springbooyvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooyvalidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooyvalidateApplication.class, args);
+    }
+
+}
---
+++ springbootfreemarker/src/test/java/com/bjsxt/springbooyvalidate/SpringbooyvalidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjsxt.springbooyvalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooyvalidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
