0237c208001da02cf0ec5751ca631c17a1dd81b9
wanghao20 <wanghao20@baijiahulian.com>
2020-08-16 21:39:23 +0800

    添加依赖，修改SpringBoot版本，在启动类上添加注解

--- springbootactuatorserver/src/main/java/com/bjsxt/springbootactuatorserver/SpringbootactuatorserverApplication.java
+++ springbootactuatorserver/src/main/java/com/bjsxt/springbootactuatorserver/SpringbootactuatorserverApplication.java
@@ -1,9 +1,17 @@
 package com.bjsxt.springbootactuatorserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author 王浩
+ */
 @SpringBootApplication
+// 开启SpringBoot Admin 服务端
+@EnableAdminServer
 public class SpringbootactuatorserverApplication {
 
     public static void main(String[] args) {
