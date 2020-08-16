9a6a85e5cc2e4b4f99b62381e640820cb02337e5
wanghao20 <wanghao20@baijiahulian.com>
2020-08-16 18:50:50 +0800

    创建springbootactuatorserver模块

---
+++ springbootactuatorserver/src/main/java/com/bjsxt/springbootactuatorserver/SpringbootactuatorserverApplication.java
@@ -0,0 +1,13 @@
+package com.bjsxt.springbootactuatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootactuatorserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootactuatorserverApplication.class, args);
+    }
+
+}
